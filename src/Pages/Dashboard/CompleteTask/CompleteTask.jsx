66979158528d49e9df4b1342945d1943932d03d6
musfikanoni <musfikanoni@gmail.com>
2025-02-22 17:03:24 +0600

    created Done & My task card

--- src/Pages/Dashboard/CompleteTask/CompleteTask.jsx
+++ src/Pages/Dashboard/CompleteTask/CompleteTask.jsx
@@ -1,8 +1,20 @@
+import useTask from "../../../hooks/useTask";
+import TaskCard from "../../Shared/TaskCard/TaskCard";
 
 const CompleteTask = () => {
+    const[taskList] = useTask();
+    const todoTask = taskList.filter(task => task.category === 'Done');
+
     return (
         <div>
-            complete Task
+            <div className="grid md:grid-cols-2 grid-cols-1 gap-7">
+                {
+                    todoTask.map(task => <TaskCard
+                    key={task._id}
+                    task={task}
+                    ></TaskCard>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/MyTask/MyTask.jsx
+++ src/Pages/Dashboard/MyTask/MyTask.jsx
@@ -1,8 +1,20 @@
+import useTask from "../../../hooks/useTask";
+import TaskCard from "../../Shared/TaskCard/TaskCard";
 
 const MyTask = () => {
+    const[taskList] = useTask();
+    
     return (
         <div>
-            My Task
+            My All Task
+            <div className="grid md:grid-cols-2 grid-cols-1 gap-7">
+                {
+                    taskList.map(task => <TaskCard
+                    key={task._id}
+                    task={task}
+                    ></TaskCard>)
+                }
+            </div>
         </div>
     );
 };
